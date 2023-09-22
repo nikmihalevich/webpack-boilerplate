031f6066b30f0d88653bd9aab5e2769ef8341ace
nikmihalevich <nikandr33@inbox.ru>
2023-09-22 16:04:03 +0300

    add jest, jest html reports, configure config, support alias and classNames test

---
+++ config/jest/jest.config.ts
@@ -0,0 +1,41 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+import path from 'path';
+
+export default {
+	globals: {
+		__IS_DEV__: true,
+		__API__: '',
+		__PROJECT__: 'jest',
+	},
+	clearMocks: true,
+	testEnvironment: 'jsdom',
+	coveragePathIgnorePatterns: ['\\\\node_modules\\\\'],
+	moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'node'],
+	moduleDirectories: ['node_modules'],
+	modulePaths: ['<rootDir>src'],
+	// May be has difference between mac os and win
+	testMatch: ['<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)'],
+	rootDir: '../../',
+	setupFilesAfterEnv: ['<rootDir>config/jest/setupTests.ts'],
+	moduleNameMapper: {
+		'\\.s?css$': 'identity-obj-proxy',
+		'\\.svg': path.resolve(__dirname, 'jestEmptyComponent.tsx'),
+		'^@/(.*)$': '<rootDir>src/$1',
+	},
+	reporters: [
+		'default',
+		[
+			'jest-html-reporters',
+			{
+				publicPath: '<rootDir>/reports/unit',
+				filename: 'report.html',
+				// openReport: true,
+				inlineSource: true,
+			},
+		],
+	],
+};
